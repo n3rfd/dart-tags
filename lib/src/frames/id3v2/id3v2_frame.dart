338e4330ecaa668630b55e2728fd5c8a54285320
Neftali Papelleras <neftali.papelleras@fullspeedtechnologies.com>
2020-07-18 22:07:32 +0800

    Find last invalid key and use it

--- lib/src/frames/id3v2/id3v2_frame.dart
+++ lib/src/frames/id3v2/id3v2_frame.dart
@@ -20,9 +20,20 @@ abstract class ID3V2Frame<T> implements Frame<T> {
     // if (bytes.length > 3 && bytes[0] == 0xFF && bytes[1] == 0xFE) {
     //   bytes = bytes.sublist(2);
     // }
-    if (bytes.length > 3 && bytes[0] == 0xFF) {
-      bytes = bytes.sublist(1);
+    // if (bytes.length > 3 && bytes[0] == 0xFF) {
+    //   bytes = bytes.sublist(1);
+    // }
+
+    var key = 0;
+
+    for (var i = 0; i < bytes.length; i++) {
+      if (bytes[i] == 0xFE) {
+        key = i;
+      }
     }
+
+    bytes = bytes.sublist(key + 1);
+
     return bytes.where((i) => i != 0).toList();
   }
 
