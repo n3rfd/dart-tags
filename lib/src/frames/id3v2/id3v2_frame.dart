a3f9f69ed96b06f70f78b0f2a2e91d31abf9df4a
Neftali Papelleras <neftali.papelleras@fullspeedtechnologies.com>
2020-07-19 21:10:34 +0800

    Trial 6

--- lib/src/frames/id3v2/id3v2_frame.dart
+++ lib/src/frames/id3v2/id3v2_frame.dart
@@ -30,9 +30,11 @@ abstract class ID3V2Frame<T> implements Frame<T> {
 
     // bytes = bytes.sublist(key + 1);
 
-    if (bytes.length > 5 && bytes[3] == 0xFF && bytes[4] == 0xFE) {
-      bytes = bytes.sublist(5);
-    }
+    // if (bytes.length > 5 && bytes[3] == 0xFF && bytes[4] == 0xFE) {
+    //   bytes = bytes.sublist(5);
+    // }
+    bytes = bytes.sublist(3);
+
     return bytes.where((i) => i != 0).toList();
     // return bytes;
   }
